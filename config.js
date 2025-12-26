3e4cee5207b9a6f527bcd0078d53d9c65fb5e8fc
Daviot <daviot@live.at>
2025-12-26 23:32:36 +0100

    feat: add more warnings

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 import { existsSync } from "node:fs";
+import { warning } from "./log";
 
 export async function readConfig(filePath) {
 	if (!existsSync(filePath)) {
-		console.log(`Configuration file ${filePath} does not exist.`);
+		warning(`Configuration file ${filePath} does not exist.`);
 		return undefined;
 	}
 	const config = await import(filePath);
 	if (!config?.default) {
-		console.log(`Configuration file ${filePath} does not export default.`);
+		warning(`Configuration file ${filePath} does not export default.`);
 		return undefined;
 	}
 	return config?.default;
